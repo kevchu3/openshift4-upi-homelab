8c1382d132f7cc5675cd9fbc740f343f7c30541b
Kevin Chung <kevin.chung@redhat.com>
2020-03-10 15:21:58 -0400

    Updating removal of .openshift_install_state.json to manifests script

--- create-ignition-configs.sh
+++ create-ignition-configs.sh
@@ -4,7 +4,5 @@ rm bootstrap.ign
 rm master.ign
 rm metadata.json
 rm worker.ign
-rm .openshift_install.log
-rm .openshift_install_state.json
 cp save/install-config.yaml .
 openshift-install create ignition-configs --dir=.
--- create-manifests-config.sh
+++ create-manifests-config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm .openshift_install.log
+rm .openshift_install_state.json
 cp save/install-config.yaml .
 openshift-install create manifests --dir=.
